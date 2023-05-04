60827dee3a7232eec8d72dd489752c52fb749922
mariokarajallo <mariokarajallo92@gmail.com>
2023-05-04 17:31:48 -0400

    feat: adds function spinner()

--- js/app.js
+++ js/app.js
@@ -120,4 +120,16 @@ function limpiarHTML() {
   }
 }
 
-function mostrarSpinner() {}
+function mostrarSpinner() {
+  limpiarHTML();
+
+  const spinner = document.createElement("DIV");
+  spinner.classList.add("spinner");
+  spinner.innerHTML = `
+  <div class="bounce1"></div>
+  <div class="bounce2"></div>
+  <div class="bounce3"></div>
+  `;
+
+  resultado.appendChild(spinner);
+}
